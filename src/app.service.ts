8893fd9388b7d905651265973a9d6a7356c259c9
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-04-01 18:17:10 +0100

    feat: update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Teevil is working fine';
+    return 'Music Booking App is working fine';
   }
 }
